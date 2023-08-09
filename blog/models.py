461c50c44fd5301f876d66c29f30be700d4a4af7
simonari <vsimonari@gmail.com>
2023-08-09 17:41:51 +0700

    Fix typo

--- blog/models.py
+++ blog/models.py
@@ -10,7 +10,7 @@ class Post(models.Model):
     )
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250,
-                            unique_for_date='publish')
+                            unique_for_date='published')
     author = models.ForeignKey(User,
                                on_delete=models.CASCADE,
                                related_name='blog_posts')
