0546bb8e68b6a1707cf2891b911f92b9773416dc
simonari <vsimonari@gmail.com>
2023-08-09 17:41:59 +0700

    Added functionality

--- blog/admin.py
+++ blog/admin.py
@@ -5,3 +5,9 @@ from .models import Post
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     list_display = ('title', 'slug', 'author', 'published', 'status')
+    list_filter = ('status', 'created', 'published')
+    search_fields = ('title', 'body')
+    prepopulated_fields = {'slug': ('title',)}
+    raw_id_fields = ('author',)
+    date_hierarchy = 'published'
+    ordering = ('status', 'published')
