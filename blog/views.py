b4281a558baaade28334922e05d2d6ff2677ba34
simonari <vsimonari@gmail.com>
2023-08-09 18:45:04 +0700

    post_list reworked in Class-based view

--- blog/views.py
+++ blog/views.py
@@ -1,23 +1,15 @@
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
 from .models import Post
 
 
-def post_list(request):
-    objects_list = Post.publish.all()
-    paginator = Paginator(objects_list, 3)
-    page = request.GET.get('page')
-
-    try:
-        posts = paginator.page(page)
-    except PageNotAnInteger:
-        posts = paginator.page(1)
-    except EmptyPage:
-        posts = paginator.page(paginator.num_pages)
-
-    return render(request,
-                  'blog/post/list.html',
-                  {'posts': posts})
+class PostListView(ListView):
+    queryset = Post.publish.all()
+    print(queryset)
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
 
 
 def post_detail(request, year, month, day, post):
