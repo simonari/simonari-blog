b425d159772c8939d1f51f1118366a18bbad6210
simonari <vsimonari@gmail.com>
2023-08-09 15:48:11 +0700

    Small fixes

--- blog/models.py
+++ blog/models.py
@@ -15,7 +15,7 @@ class Post(models.Model):
                                on_delete=models.CASCADE,
                                related_name='blog_posts')
     body = models.TextField()
-    publish = models.DateTimeField(default=timezone.now)
+    published = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=10,
@@ -23,7 +23,7 @@ class Post(models.Model):
                               default='draft')
 
     class Meta:
-        ordering = ('-publish',)
+        ordering = ('-published',)
 
     def __str__(self):
         return self.title
