76c7a25988cbca79b798efe313c3cd3fa076a038
simonari <vsimonari@gmail.com>
2023-08-12 14:49:14 +0700

    Added base class for detail views and inheritance from it

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,27 @@
 from django.shortcuts import get_object_or_404, redirect
 from django.views.generic import ListView, DetailView, CreateView, UpdateView
 from django.utils.text import slugify
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponseNotFound
 from .models import Post
-from .forms import DraftDetailUpdateForm
+from .forms import DraftDetailUpdateForm, PostDetailUpdateForm
+
+STATUS_TO_SLUG = {
+    'posted': 'post',
+    'drafted': 'draft'
+}
+
+STATUS_ANTIPODE = {
+    'posted': 'drafted',
+    'drafted': 'posted'
+}
+
+
+class PostListView(ListView):
+    queryset = Post.objects.filter(status='posted')
+    context_object_name = 'posts'
+
+    paginate_by = 5
+    template_name = 'blog/post/list.html'
 
 
 class DraftListView(ListView):
@@ -14,67 +32,68 @@ class DraftListView(ListView):
     template_name = 'blog/draft/list.html'
 
 
-class DraftDetailView(UpdateView):
+class BaseDetailView(UpdateView):
     model = Post
-    context_object_name = "draft"
-    form_class = DraftDetailUpdateForm
-    template_name = 'blog/draft/detail.html'
-    success_url = None
-    object = None
 
-    def get_object(self, **kwargs):
+    context_object_name = None
+    form_class = None
+    template_name = None
+
+    object = None
+    object_status = None
+
+    def get_object(self, queryset=None):
+        slug = self._status_to_slug(self.object_status)
+
         obj = get_object_or_404(Post,
-                                slug=self.kwargs['draft'],
-                                status='draft',
+                                slug=slug,
+                                status=self.object_status,
                                 published__year=self.kwargs['year'],
                                 published__month=self.kwargs['month'],
                                 published__day=self.kwargs['day'])
         self.object = obj
         return obj
 
-    def form_valid(self, form):
-        self.object.status = 'published'
+    def _status_to_slug(self, status):
+        slug = STATUS_TO_SLUG.get(status)
+
+        if slug is None:
+            raise ValueError(f'Status {status} is invalid')
+
+        return self.kwargs[slug]
+
+    def _set_status(self, status):
+        self.object.status = status
         self.object.save()
-        self.success_url = self.object.get_absolute_url()
-        return HttpResponseRedirect(self.success_url)
+
+    def _change_status(self):
+        status_to_set = STATUS_ANTIPODE.get(self.object.status)
+
+        if not status_to_set:
+            raise ValueError(f'Status {self.object.status} can not be changed')
+
+        self._set_status(status_to_set)
+
+    def form_valid(self, form):
+        self._change_status()
+        url_redirect = self.object.get_absolute_url()
+        return HttpResponseRedirect(url_redirect)
 
 
-    # def btn_publish(self):
-    #     obj = get_object_or_404(Post,
-    #                             slug=self.kwargs['draft'],
-    #                             status='draft',
-    #                             published__year=self.kwargs['year'],
-    #                             published__month=self.kwargs['month'],
-    #                             published__day=self.kwargs['day'])
-    #
-    #     obj.status = 'published'
-    #     obj.save()
-    #     redirect(obj.get_absolute_url())
+class DraftDetailView(BaseDetailView):
+    context_object_name = "draft"
+    form_class = DraftDetailUpdateForm
+    template_name = 'blog/draft/detail.html'
+
+    object_status = 'drafted'
 
 
-class PostListView(ListView):
-    queryset = Post.publish.all()
-    context_object_name = 'posts'
-
-    paginate_by = 5
-    template_name = 'blog/post/list.html'
-
-
-class PostDetailView(DetailView):
-    model = Post
+class PostDetailView(BaseDetailView):
     context_object_name = 'post'
-
     template_name = 'blog/post/detail.html'
 
-    def get_object(self, **kwargs):
-        obj = get_object_or_404(Post,
-                                slug=self.kwargs['post'],
-                                status='published',
-                                published__year=self.kwargs['year'],
-                                published__month=self.kwargs['month'],
-                                published__day=self.kwargs['day'])
-
-        return obj
+    form_class = PostDetailUpdateForm
+    object_status = 'posted'
 
 
 class PostCreateView(CreateView):
