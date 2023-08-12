7b98e2c4899afb87dee3a75c7a7c02733b3e7f58
simonari <vsimonari@gmail.com>
2023-08-12 13:38:03 +0700

    Added draft detail view

--- blog/urls.py
+++ blog/urls.py
@@ -4,10 +4,14 @@ from . import views
 app_name = 'blog'
 
 urlpatterns = [
+    # posts views
     path('', views.PostListView.as_view(), name='post_list'),
     path('<int:year>/<int:month>/<int:day>/<slug:post>',
-         views.PostDetailView.as_view(),
-         name='post_detail'),
+         views.PostDetailView.as_view(), name='post_detail'),
     path('new-post', views.PostCreateView.as_view(), name='post_create'),
+
+    # drafts views
     path('draft', views.DraftListView.as_view(), name='draft_list'),
+    path('draft/<int:year>/<int:month>/<int:day>/<slug:draft>',
+         views.DraftDetailView.as_view(), name='draft_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,14 +1,62 @@
-from django.shortcuts import get_object_or_404
-from django.views.generic import ListView, DetailView, CreateView
+from django.shortcuts import get_object_or_404, redirect
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
 from django.utils.text import slugify
+from django.http import HttpResponseRedirect
 from .models import Post
+from .forms import DraftDetailUpdateForm
+
+
+class DraftListView(ListView):
+    queryset = Post.objects.filter(status='draft')
+    context_object_name = "drafts"
+
+    paginate_by = 5
+    template_name = 'blog/draft/list.html'
+
+
+class DraftDetailView(UpdateView):
+    model = Post
+    context_object_name = "draft"
+    form_class = DraftDetailUpdateForm
+    template_name = 'blog/draft/detail.html'
+    success_url = None
+    object = None
+
+    def get_object(self, **kwargs):
+        obj = get_object_or_404(Post,
+                                slug=self.kwargs['draft'],
+                                status='draft',
+                                published__year=self.kwargs['year'],
+                                published__month=self.kwargs['month'],
+                                published__day=self.kwargs['day'])
+        self.object = obj
+        return obj
+
+    def form_valid(self, form):
+        self.object.status = 'published'
+        self.object.save()
+        self.success_url = self.object.get_absolute_url()
+        return HttpResponseRedirect(self.success_url)
+
+
+    # def btn_publish(self):
+    #     obj = get_object_or_404(Post,
+    #                             slug=self.kwargs['draft'],
+    #                             status='draft',
+    #                             published__year=self.kwargs['year'],
+    #                             published__month=self.kwargs['month'],
+    #                             published__day=self.kwargs['day'])
+    #
+    #     obj.status = 'published'
+    #     obj.save()
+    #     redirect(obj.get_absolute_url())
 
 
 class PostListView(ListView):
     queryset = Post.publish.all()
     context_object_name = 'posts'
 
-    paginate_by = 3
+    paginate_by = 5
     template_name = 'blog/post/list.html'
 
 
@@ -41,5 +89,6 @@ class PostCreateView(CreateView):
         return super().form_valid(form)
 
 
-def post_edit(request, year, month, day, post):
+# TODO inherit from EditView
+class PostEditView:
     ...
